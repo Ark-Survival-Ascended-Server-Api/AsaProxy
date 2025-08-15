1bfad2077d32819c77b59b3da92b673d5a97fab4
jimmy-mll <jimmy.mll@hotmail.com>
2025-08-15 09:18:43 +0200

    feat: add Dotnet integration with hostfxr runtime and initialization logic

---
+++ include/Dotnet.hpp
@@ -0,0 +1,85 @@
+﻿#pragma once
+
+#include <filesystem>
+#include <mutex>
+#include <memory>
+#include <atomic>
+#include <thread>
+#include <hostfxr.h>
+#include <coreclr_delegates.h>
+
+#include "Path.hpp"
+
+namespace fs = std::filesystem;
+
+typedef void (CORECLR_DELEGATE_CALLTYPE* main_fn)();
+
+class dotnet
+{
+public:
+    explicit dotnet(const fs::path& dotnet_runtime_config_path);
+
+    dotnet(const dotnet&) = delete;
+
+    dotnet& operator=(const dotnet&) = delete;
+
+    dotnet(dotnet&&) = delete;
+
+    dotnet& operator=(dotnet&&) = delete;
+
+    ~dotnet();
+
+    void call_function_with_unmanaged_callers_only(const std::wstring& assembly_path, const std::string& type_name, const std::string& method_name) const;
+
+private:
+    mutable std::mutex mutex_;
+    hostfxr_handle cxt_;
+    load_assembly_and_get_function_pointer_fn load_assembly_and_get_function_pointer_;
+    hostfxr_initialize_for_runtime_config_fn init_f_ptr_;
+    hostfxr_get_runtime_delegate_fn get_delegate_f_ptr_;
+    hostfxr_close_fn close_f_ptr_;
+};
+
+inline std::unique_ptr<dotnet> g_dotnet;
+inline std::atomic<bool> g_dotnet_initialized;
+inline std::thread g_init_thread;
+inline std::mutex g_init_mutex;
+
+inline void start_dotnet()
+{
+    Sleep(200);
+
+    const auto asa_dll_path = get_asa_dll_path();
+    const auto asa_runtime_config_path = get_asa_runtime_config_path();
+
+    if (!fs::exists(asa_dll_path))
+        throw std::runtime_error("ASA DLL not found: " + asa_dll_path.string());
+
+    if (!fs::exists(asa_runtime_config_path))
+        throw std::runtime_error("ASA runtime config not found: " + asa_runtime_config_path.string());
+
+    {
+        std::lock_guard lock(g_init_mutex);
+        g_dotnet = std::make_unique<dotnet>(asa_runtime_config_path);
+    }
+
+    g_dotnet->call_function_with_unmanaged_callers_only(asa_dll_path.wstring(), "AsaApi.Program, AsaApi", "Main");
+    g_dotnet_initialized = true;
+}
+
+inline void initialize_dotnet()
+{
+    if (auto expected = false; g_dotnet_initialized.compare_exchange_strong(expected, true))
+    {
+        g_init_thread = std::thread(start_dotnet);
+        g_init_thread.detach();
+    }
+}
+
+inline void free_dotnet()
+{
+    std::lock_guard lock(g_init_mutex);
+
+    if (g_dotnet)
+        g_dotnet.reset();
+}
---
+++ src/Dotnet.cpp
@@ -0,0 +1,69 @@
+﻿#include "Dotnet.hpp"
+
+#include <Windows.h>
+#include <nethost.h>
+
+dotnet::dotnet(const fs::path& dotnet_runtime_config_path)
+{
+    std::lock_guard lock(mutex_);
+
+    wchar_t buffer[MAX_PATH];
+    size_t buffer_size = sizeof(buffer) / sizeof(wchar_t);
+
+    if (const auto rc = get_hostfxr_path(buffer, &buffer_size, nullptr); rc != 0)
+        throw std::runtime_error("Failed to get hostfxr path");
+
+    const auto lib = LoadLibraryW(buffer);
+
+    if (!lib)
+        throw std::runtime_error("Failed to load hostfxr");
+
+    init_f_ptr_ = (hostfxr_initialize_for_runtime_config_fn)GetProcAddress(lib, "hostfxr_initialize_for_runtime_config");
+    get_delegate_f_ptr_ = (hostfxr_get_runtime_delegate_fn)GetProcAddress(lib, "hostfxr_get_runtime_delegate");
+    close_f_ptr_ = (hostfxr_close_fn)GetProcAddress(lib, "hostfxr_close");
+
+    if (!init_f_ptr_ || !get_delegate_f_ptr_ || !close_f_ptr_)
+        throw std::runtime_error("Failed to get hostfxr functions");
+
+    if (const auto rc = init_f_ptr_(dotnet_runtime_config_path.wstring().c_str(), nullptr, &cxt_); rc != 0 || cxt_ == nullptr)
+        throw std::runtime_error("Failed to initialize hostfxr");
+
+    if (const auto rc = get_delegate_f_ptr_(cxt_, hdt_load_assembly_and_get_function_pointer, (void**)&load_assembly_and_get_function_pointer_); rc != 0 || load_assembly_and_get_function_pointer_ == nullptr)
+        throw std::runtime_error("Failed to get runtime delegate");
+}
+
+dotnet::~dotnet()
+{
+    std::lock_guard lock(mutex_);
+
+    if (cxt_ && close_f_ptr_)
+    {
+        close_f_ptr_(cxt_);
+        cxt_ = nullptr;
+    }
+}
+
+[[noreturn]]
+void dotnet::call_function_with_unmanaged_callers_only(const std::wstring& assembly_path, const std::string& type_name, const std::string& method_name) const
+{
+    std::lock_guard lock(mutex_);
+
+    const auto assembly_path_str = std::wstring(assembly_path.begin(), assembly_path.end());
+    const auto type_name_str = std::wstring(type_name.begin(), type_name.end());
+    const auto method_name_str = std::wstring(method_name.begin(), method_name.end());
+
+    const main_fn fn = nullptr;
+
+    const auto rc = load_assembly_and_get_function_pointer_(
+                assembly_path_str.c_str(),
+                type_name_str.c_str(),
+                method_name_str.c_str(),
+                UNMANAGEDCALLERSONLY_METHOD,
+                nullptr,
+                reinterpret_cast<void**>(fn));
+
+    if (rc != 0 || *fn == nullptr)
+        throw std::runtime_error("Failed to load assembly or get function pointer");
+
+    fn();
+}
