927cdda8ccaa5922bfa8051619f0ac3903bdc920
jimmy-mll <jimmy.mll@hotmail.com>
2025-08-15 09:18:23 +0200

    feat: add utility functions for file and directory paths in Path.hpp

---
+++ include/Path.hpp
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include <filesystem>
+#include <Windows.h>
+
+namespace fs = std::filesystem;
+
+inline fs::path get_exe_path()
+{
+    wchar_t buffer[MAX_PATH];
+    GetModuleFileNameW(nullptr, buffer, MAX_PATH);
+    return fs::path(buffer).parent_path();
+}
+
+inline fs::path get_asa_directory()
+{
+    return get_exe_path() / "AsaApi";
+}
+
+inline fs::path get_asa_dll_path()
+{
+    return get_asa_directory() / "AsaApi.dll";
+}
+
+inline fs::path get_asa_runtime_config_path()
+{
+    return get_asa_directory() / "AsaApi.runtimeconfig.json";
+}
+
+inline fs::path get_system_directory()
+{
+    wchar_t buffer[MAX_PATH];
+    GetSystemDirectoryW(buffer, MAX_PATH);
+    return buffer;
+}
+
+inline fs::path get_system_version_dll_path()
+{
+    return get_system_directory() / "version.dll";
+}
